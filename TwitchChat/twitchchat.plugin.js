cd898bd406dabed59001020ef084eb51d78e8ef0
Strencher <46447572+Strencher@users.noreply.github.com>
2019-11-15 22:46:44 +0100

    Update twitchchat.plugin.js

--- TwitchChat/twitchchat.plugin.js
+++ TwitchChat/twitchchat.plugin.js
@@ -4,7 +4,7 @@ class twitchChat {
   getName() { return "Twitch Chat"; }
   getAuthor() { return "Strencher"; }
   getDescription() { return "Adds a twitch chat to Discord."; }
-  getVersion() { return "0.0.0"; }
+  getVersion() { return "0.0.1"; }
   load() {
     ZLibrary.PluginUpdater.checkForUpdate("Twitch Chat", this.getVersion(), "https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/TwitchChat/twitchchat.plugin.js");
     this.loadSettings();
