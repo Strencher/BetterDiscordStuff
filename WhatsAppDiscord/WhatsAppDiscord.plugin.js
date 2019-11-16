e2712faa9d268517cf4a73c8077e78e1a741359b
Strencher <florentine110405@gmail.com>
2019-11-16 19:34:49 +0100

    PogChamp

---
+++ WhatsAppDiscord/WhatsAppDiscord.plugin.js
@@ -0,0 +1,49 @@
+//META{"name":"whatsAppDiscord"}*//
+class whatsAppDiscord {
+  initConstructor() { }
+  getName() { return "!!!!WhatsApp Discord"; }
+  getAuthor() { return "Strencher"; }
+  getDescription() { return "Adds a WhatsApp window to Discord."; }
+  getVersion() { return "0.0.0"; }
+  load() {}
+  start() {
+      if(document.getElementById("whatsAppButton")){
+          return;
+        } else {
+            this.Button()
+        }
+    }
+  onSwitch() {
+    if(document.getElementById("whatsAppButton")){
+        return;
+    } else {
+        this.Button()
+    }
+}
+  stop() {}
+  Button() {
+    let inner = document.getElementsByClassName("toolbar-1t6TWx")[0];
+    let button = document.createElement("button");
+    let Icon = document.createElement("img");
+    let buttonInner = document.createElement("div");
+    Icon.setAttribute("src", "https://image.flaticon.com/icons/svg/220/220236.svg");
+    Icon.height = "25";
+    Icon.width = "25";
+    button.id = "whatsAppButton";
+    button.style = "background-color: transparent !important;";
+    buttonInner.style = "background-color: transparent !important;";
+    inner.appendChild(buttonInner);
+    buttonInner.appendChild(button);
+    button.appendChild(Icon);
+    Icon.onmouseover = () => {
+      Icon.setAttribute("style", "transform: scale(1.2);")
+    }
+    Icon.onmouseout = () => {
+      Icon.setAttribute("style", "transform: scale(1);")
+    }
+    button.onclick = () => {
+      window.open("whatsapp://");
+    }
+  }
+
+}
---
+++ WhatsAppDiscord/readme.md
@@ -0,0 +1,4 @@
+# WhatsApp Discord
+## Download [**🔽WhatsApp Discord**](https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/WhatsAppDiscord/WhatsAppDiscord.plugin.js) Plugin
+
+**!!Requires WhatsApp Desktop App!!**
