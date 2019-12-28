074d6cf63a5ecf0f9a4d6e03d4486b6008dfce9d
Strencher <46447572+Strencher@users.noreply.github.com>
2019-12-28 17:06:00 +0100

    my lib

--- Guild-Counter/Guild-counter.plugin.js
+++ Guild-Counter/Guild-counter.plugin.js
@@ -7,70 +7,105 @@ class guildCounter {
         return "Strencher";
     }
     getDescription() {
-        return 'Displays a count from your server. \nPut \n:root {\n--GC-color: red;\n} \nInto CustomCSS editor to change the color.';
+        return 'Support-server: https://discord.gg/gvA2ree \nDisplays a count from your server. \nPut \n:root {\n--GC-color: red;\n} \nInto CustomCSS editor to change the color.';
     }
     getVersion() {
-        return "0.0.1";
+        return "0.0.2";
     }
     load() {
         ZLibrary.PluginUpdater.checkForUpdate("Guild-Counter", this.getVersion(), "https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/Guild-Counter/Guild-counter.plugin.js");
-        let NaJib = document.getElementById("NaJibLibrary");
-        if (!NaJib) {
-            najib = document.createElement("script");
-            najib.id = "NaJibLibrary";
-            najib.type = "text/javascript";
-            najib.src = "https://strencher.github.io/NaJib.js";
-            document.head.appendChild(najib);
-        }
     }
+
     unload() {
         this.stop()
     }
     start() {
-        window.setTimeout(() => {
-            let count = BdApi.findModuleByProps('totalGuilds').totalGuilds;
-            let a = $(`<div class="Guild-counter">${count}</div>`);
-            a.appendTo(".guildSeparator-3s64Iy");
-            a.on("click", () => {
-                NaJib.showToast(`Your'e currently in ${count} servers.`, {
-                    type: "info",
-                    timeout: "5000",
-                    onclick: null
-                })
+        if (!global.NaJib) {
+            pluginModule.stopPlugin("NaJibLibrary")
+            const title = "Library Missing";
+            const ModalStack = BdApi.findModuleByProps("push", "update", "pop", "popWithKey");
+            const TextElement = BdApi.findModuleByProps("Sizes", "Weights");
+            const ConfirmationModal = BdApi.findModule(m => m.defaultProps && m.key && m.key() == "confirm-modal");
+            if (!ModalStack || !ConfirmationModal || !TextElement) return BdApi.alert(title, `The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/Lib/0NaJibLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+            ModalStack.push(function (props) {
+                return BdApi.React.createElement(ConfirmationModal, Object.assign({
+                    header: title,
+                    children: [TextElement({ color: TextElement.Colors.PRIMARY, children: [`The NaJib library plugin needed for ${this.getName()} is missing. Please click Download Now to install it.`] })],
+                    red: false,
+                    confirmText: "Download Now",
+                    cancelText: "Cancel",
+                    onConfirm: () => {
+                        require("request").get("https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/Lib/0NaJibLibrary.plugin.js", async (error, response, body) => {
+                            if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/Lib/0NaJibLibrary.plugin.js");
+                            await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0NaJibLibrary.plugin.js"), body, r));
+                        });
+                    }
+                }, props));
+            });
+        } else {
+            try {
+                this.libLoaded()
+            }
+            catch (err) { setTimeout(() => { pluginModule.startPlugin("NaJibLibrary") }, 10000) }
+        }
+    }
+    libLoaded() {
+        let base = document.querySelectorAll(".guildSeparator-3s64Iy")[0];
+        let count = NaJib.Modules.findAllByProps('totalGuilds').totalGuilds;
+        let h1 = document.createElement("h1");
+        h1.className = "Guild-counter";
+        h1.innerHTML = `${count}`;
+        base.appendChild(h1);
+        base.addEventListener("click", () => {
+            count = NaJib.Modules.findAllByProps('totalGuilds').totalGuilds;
+            h1.innerHTML = `${count}`;
+            NaJib.showToast(`Your'e currently in ${count} servers.`, {
+                type: "info",
+                timeout: "5000",
+                onclick: null
             })
-            if (!document.getElementById("Guild-counterCSS")) {
-                NaJib.injectCSS("Guild-counterCSS", `
-            :root {
-                --GC-color: #36393f;
-            }
-            .guildSeparator-3s64Iy {
-              height: 15px;
-              color: #ffffff;
-              width: 52px;
-              border-top-left-radius: 7px;
-              border-bottom-left-radius: 7px;
-              border-bottom-right-radius: 7px;
-              border-top-right-radius: 7px;
-              background-color: var(--GC-color);
-            }
-            .Guild-counter {
-              color: white;
-              cursor: pointer;
-              margin-left: 18px;
-              border-bottom-right-radius: 7px;
-              border-top-right-radius: 7px;
-              background-color: var(--GC-color);
-            }
-            li[data-name="Guild-Counter"] .bda-description {
-                white-space: pre;
-            }
-            `)
-            }
-        }, 1200);
+        })
+        if (!document.getElementById("Guild-counterCSS")) {
+            NaJib.injectCSS("Guild-counterCSS", `
+                
+                :root {
+                    --GC-color: #36393f;
+                }
+                .guildSeparator-3s64Iy {
+                  height: 15px;
+                  width: 52px;
+                  background-color: var(--GC-color);
+                }
+                .Guild-counter {
+                  color: white;
+                  cursor: pointer;
+                  text-align: center;
+                  background-color: var(--GC-color);
+                }
+                .GC-inner {
+                  background-color: var(--GC-color);
+				  transition: 0.2s;
+                }
+                li[data-name="Guild-Counter"] .bda-description {
+                    white-space: pre;
+                }
+                .GC-inner:hover {
+				  filter: brightness(125%);
+				  transition: 0.2s;
+				}
+                `)
+        }
 
     }
     stop() {
-        NaJib.deleteElement("#Guild-counterCSS");
-        NaJib.deleteElement(".Guild-counter")
+        if (document.getElementById("Guild-counterCSS")) {
+
+            NaJib.ElementOptions.delete("#Guild-counterCSS");
+        }
+        if (document.getElementsByClassName("Guild-counter")) {
+
+            NaJib.ElementOptions.delete(".Guild-counter")
+        }
+        document.querySelectorAll(".guildSeparator-3s64Iy")[0].removeEventListener("click", this.event)
     }
 }
