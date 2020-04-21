1fdf94837d535fbc9ff8b86354a37da85257b949
Strencher <46447572+Strencher@users.noreply.github.com>
2020-04-21 12:02:10 +0200

    new plugin

---
+++ VoiceUsersCounter/VoiceUsersCounter.plugin.js
@@ -0,0 +1,172 @@
+/**
+* @name VoiceUsersCounter
+* @displayName VoiceUsersCounter
+* @authorId 415849376598982656
+* @invite gvA2ree
+*/
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+
+@else@*/
+
+const VoiceUsersCounter = (() => {
+    const config = {
+        info: {
+            name: "VoiceUsersCounter",
+            authors: [
+                {
+                    name: "Strencher",
+                    discord_id: "415849376598982656",
+                    github_username: "Strencher",
+                    twitter_username: "Strencher3"
+                }
+            ],
+            version: "0.0.1",
+            description: "Adds a count of users they're connected to a VoiceChannel. Customize the Color of the count in the settingspanel when your\'e connected to the voicechannel.",
+            github: "https://github.com/Strencher/BetterDiscordStuff/blob/master/VoiceUsersCounter/VoiceUsersCounter.plugin.js",
+            github_raw: "https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/VoiceUsersCounter/VoiceUsersCounter.plugin.js"
+        },
+        changelog: [
+            {
+                title: "Yeah",
+                type: "added",
+                items: ["The plugin exist"]
+            }
+        ],
+        defaultConfig: [
+            {
+                type: "color",
+                id: "color",
+                value: "#7289DA",
+                name: "NumberColor",
+                note: "Customize the color of the number when you are in the VoiceChannel"
+            }
+        ]
+    };
+
+    return !global.ZeresPluginLibrary ? class {
+        constructor() { this._config = config; }
+        getName() { return config.info.name; }
+        getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
+        getDescription() { return config.info.description; }
+        getVersion() { return config.info.version; }
+        load() {
+            BdApi.showConfirmationModal("Library plugin is needed", 
+                [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`], {
+                    confirmText: "Download",
+                    cancelText: "Cancel",
+                    onConfirm: () => {
+                        require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                        });
+                    }
+                });
+        }
+        start() { }
+        stop() { }
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Api) => {
+
+            const { WebpackModules, DiscordAPI, Utilities, PluginUtilities, DiscordModules, ReactComponents, Patcher, DiscordSelectors, ReactTools } = Api;
+            const { React } = DiscordModules;
+            const Tooltip = WebpackModules.getByDisplayName("Tooltip")
+            class VoiceCount extends React.Component {
+                render() {
+                    return React.createElement(Tooltip, {
+                        text: "Connected Users: "+this.props.count,
+                        position: "top",
+                        color: "black"
+                    }, e => React.createElement("span", Object.assign({
+                        className: "voiceCounter", 
+                        onMouseEnter: e.onMouseEnter, 
+                        onMouseLeave: e.onMouseLeave
+                    }, this.props.options), this.props.count))
+                }
+            }
+            return class VoiceUsersCounter extends Plugin {
+                constructor() {
+                    super();
+                }
+                getSettingsPanel() {
+                    const panel = this.buildSettingsPanel();
+                    panel.addListener(() => {
+                        document.querySelectorAll(DiscordSelectors.ChannelList.containerDefault).forEach(e=> {
+                            if(e) ReactTools.getOwnerInstance(e).forceUpdate()
+                        })
+                    })
+                    return panel.getElement()
+                }
+                async onStart() {
+                    PluginUtilities.addStyle(config.info.name, 
+                    `.voiceCounter {
+                        color: var(--channels-default);
+                        position: absolute;
+                        top: 8px;
+                        right: 11px;
+                        font-weight: bold; 
+                    }`)                    
+                    const VoiceChannelComponent = await ReactComponents.getComponentByName("VoiceChannel", DiscordSelectors.ChannelList.containerDefault);
+                    this.unpatch = Patcher.after(VoiceChannelComponent.component.prototype, "render", ({props}, _, ret) => {
+                        let childs = Utilities.getNestedProp(ret, "props.children");
+                        if(childs && Array.isArray(childs)) {
+                            if(Array.isArray(props.voiceStates) && props.voiceStates.find(e=> e.user.id == DiscordAPI.currentUser.id)) childs.push(React.createElement(VoiceCount, {
+                                options: {
+                                    style: {
+                                        color: this.settings.color || "var(--blurple)"
+                                    }
+                                },
+                                count: Array.isArray(props.voiceStates) ? props.voiceStates.length : "0"
+                            }))
+                            else childs.push(React.createElement(VoiceCount, {
+                                count: Array.isArray(props.voiceStates) ? props.voiceStates.length : "0"
+                            }))
+                        } else {
+                            let children = Utilities.getNestedProp(ret, "props.children.props.children");
+                            if(Array.isArray(props.voiceStates) && props.voiceStates.find(e=> e.user.id == DiscordAPI.currentUser.id)) children.push(React.createElement(VoiceCount, {
+                                options: {
+                                    style: {
+                                        color: this.settings.color || "var(--blurple)"
+                                    }
+                                },
+                                count: Array.isArray(props.voiceStates) ? props.voiceStates.length : "0"
+                            }))
+                            else children.push(React.createElement(VoiceCount, {
+                                count: Array.isArray(props.voiceStates) ? props.voiceStates.length : "0"
+                            }))
+                        }
+                    })
+                    VoiceChannelComponent.forceUpdateAll()
+                }
+                onStop() {
+                    PluginUtilities.removeStyle(config.info.name)
+                    this.unpatch()
+                }
+
+            }
+
+        };
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+
+                    
---
+++ VoiceUsersCounter/readme.md
@@ -0,0 +1,4 @@
+# VoiceUsersCounter 
+- Adds a count of users they're connected to a VoiceChannel. Customize the Color of the count in the settingspanel when your\'e connected to the voicechannel.
+## Download [**🔽VoiceUsersCounter**](https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/VoiceUsersCounter/VoiceUsersCounter.plugin.js) Plugin
+ - [Support-Server](https://discord.gg/gvA2ree)
