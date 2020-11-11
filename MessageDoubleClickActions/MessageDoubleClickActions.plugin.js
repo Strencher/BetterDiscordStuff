790b0a1cf7c9f78186b1511b91f92510df55e3e1
Strencher <46447572+Strencher@users.noreply.github.com>
2020-11-11 20:28:12 +0100

    MDA v0.0.5

--- MessageDoubleClickActions/MessageDoubleClickActions.plugin.js
+++ MessageDoubleClickActions/MessageDoubleClickActions.plugin.js
@@ -25,145 +25,144 @@
         shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
     }
     WScript.Quit();
-
 @else@*/
 
 const MessageDoubleClickActions = (() => {
-    const config = {
-        info: {
-            name: "MessageDoubleClickActions",
-            authors: [
-                {
-                    name: "Strencher",
-                    discord_id: "415849376598982656",
-                    github_username: "Strencher",
-                    twitter_username: "Strencher3"
-                }
-            ],
-            version: "0.0.4",
-            description: "Adds an Action by Double Clicking a message.",
-            github: "https://github.com/Strencher/BetterDiscordStuff/MessageDoubleClickActions/MessageDoubleClickActions.plugin.js",
-            github_raw: "https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/MessageDoubleClickActions/MessageDoubleClickActions.plugin.js"
-        },
-        changelog: [
-            {
-                type: "improved",
-                title: "Improved",
-                items: [
-                    "The plugin now uses a better method to add the eventlistener. Thanks Juby210 for reminding me of that."
-                ]
-            }
-        ],
-        defaultConfig: [
-            {
-                type: "switch",
-                name: "Edit Message",
-                value: true,
-                id: "edit",
-                note: "It does not work when you have Delete Message enabled!"
-            },
-            {
-                type: "switch",
-                name: "Delete Message",
-                value: true,
-                id: "delete",
-                note: "It does not work when you have Edit Message, Add Reaction or Copy MessageLink enabled!"
-            },
-            {
-                type: "switch",
-                name: "Copy Message Content",
-                value: true,
-                id: "copy_content"
-            },
-            {
-                type: "switch",
-                name: "Copy MessageLink",
-                value: true,
-                id: "copy_link",
-                note: "It does not work when you have Delete Message enabled!"
-            },
-            {
-                type: "switch",
-                name: "Add Reaction to Message",
-                value: true,
-                id: "add_reaction",
-                note: "It does not work when you have DeleteMessage enabled!"
-            },
-            {
-                type: "textbox",
-                value: "🔴",
-                name: "Reaction",
-                placeholder: "Reaction UNICode",
-                id: "reaction",
-            }
-        ]
-    };
+  const config = {
+      info: {
+          name: "MessageDoubleClickActions",
+          authors: [
+              {
+                  name: "Strencher",
+                  discord_id: "415849376598982656",
+                  github_username: "Strencher",
+                  twitter_username: "Strencher3"
+              }
+          ],
+          version: "0.0.5",
+          description: "Adds an Action by Double Clicking a message.",
+          github: "https://github.com/Strencher/BetterDiscordStuff/MessageDoubleClickActions/MessageDoubleClickActions.plugin.js",
+          github_raw: "https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/MessageDoubleClickActions/MessageDoubleClickActions.plugin.js"
+      },
+      changelog: [
+          {
+              type: "fixed",
+              title: "0.0.5",
+              items: [
+                  "Fixed a discord update."
+              ]
+          }
+      ],
+      defaultConfig: [
+          {
+              type: "switch",
+              name: "Edit Message",
+              value: true,
+              id: "edit",
+              note: "It does not work when you have Delete Message enabled!"
+          },
+          {
+              type: "switch",
+              name: "Delete Message",
+              value: true,
+              id: "delete",
+              note: "It does not work when you have Edit Message, Add Reaction or Copy MessageLink enabled!"
+          },
+          {
+              type: "switch",
+              name: "Copy Message Content",
+              value: true,
+              id: "copy_content"
+          },
+          {
+              type: "switch",
+              name: "Copy MessageLink",
+              value: true,
+              id: "copy_link",
+              note: "It does not work when you have Delete Message enabled!"
+          },
+          {
+              type: "switch",
+              name: "Add Reaction to Message",
+              value: true,
+              id: "add_reaction",
+              note: "It does not work when you have DeleteMessage enabled!"
+          },
+          {
+              type: "textbox",
+              value: "🔴",
+              name: "Reaction",
+              placeholder: "Reaction UNICode",
+              id: "reaction",
+          }
+      ]
+  };
 
-    return !global.ZeresPluginLibrary ? class {
-        constructor() { this._config = config; }
-        getName() { return config.info.name; }
-        getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
-        getDescription() { return config.info.description; }
-        getVersion() { return config.info.version; }
-        load() {
-            BdApi.showConfirmationModal("Library plugin is needed", 
-                [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`], {
-                    confirmText: "Download",
-                    cancelText: "Cancel",
-                    onConfirm: () => {
-                        require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
-                        });
-                    }
-                });
-        }
-        start() { }
-        stop() { }
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Api) => {
+  return !global.ZeresPluginLibrary ? class {
+      constructor() { this._config = config; }
+      getName() { return config.info.name; }
+      getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
+      getDescription() { return config.info.description; }
+      getVersion() { return config.info.version; }
+      load() {
+          BdApi.showConfirmationModal("Library plugin is needed", 
+              [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`], {
+                  confirmText: "Download",
+                  cancelText: "Cancel",
+                  onConfirm: () => {
+                      require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                      if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                      await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
+                      });
+                  }
+              });
+      }
+      start() { }
+      stop() { }
+  } : (([Plugin, Api]) => {
+      const plugin = (Plugin, Api) => {
 
-            const { Utilities, Patcher, WebpackModules, ReactTools, DiscordAPI, DiscordModules, Toasts, DiscordModules: { ElectronModule } } = Api;
-            return class MessageDoubleClickActions extends Plugin {
-                constructor() {
-                    super();
-                }
-                onStart() {
-                    const Message = WebpackModules.getModule(m=>m.default && m.default.displayName == "Message");
-                    if(Message && "default" in Message) this.unpatch = Patcher.after(Message, "default", (_, args, ret)=>{
-                        ret.props.onDoubleClick = ()=>{
-                            const props = Utilities.getNestedProp(args[0], "childrenMessageContent.props");
-                            if(!props) return;
-                            if(props && this.settings.edit && !this.settings.delete && props.message.author.id == DiscordAPI.currentUser.id) {
-                                WebpackModules.getByProps("startEditMessage").startEditMessage(props.message.channel_id, props.message.id, props.message.content)
-                            };
-                            if(props && this.settings.delete && !this.settings.edit && !this.settings.copy_link && !this.settings.add_reaction) {
-                                WebpackModules.getByProps("deleteMessage").deleteMessage(props.message.channel_id, props.message.id);
-                            };
-                            if(props && this.settings.copy_link && !this.settings.delete) {
-                                ElectronModule.copy(`https://discordapp.com/channels/${props.channel.guild_id}/${props.channel.id}/${props.message.id}`);
-                                Toasts.success("Copied Message Link");
-                            };
-                            if(props && this.settings.copy_content) {
-                                ElectronModule.copy(props.message.content);
-                                Toasts.success("Copied Message Content");
-                            };
-                            if(props && this.settings.add_reaction && !this.settings.delete) {
-                                WebpackModules.getByProps("addReaction").addReaction(props.message.channel_id, props.message.id, {name: this.settings.reaction})
-                            }    
-                        }
-                    })
-                }
-                onStop() {
-                    this.unpatch();
-                }
-                getSettingsPanel() {
-                    return this.buildSettingsPanel().getElement();
-                }
+          const {Utilities, Patcher, WebpackModules, ReactTools, DiscordAPI, DiscordModules, Toasts, DiscordModules: { ElectronModule } } = Api;
+          return class MessageDoubleClickActions extends Plugin {
+              constructor() {
+                  super();
+              }
+              onStart() {
+                  const Message = WebpackModules.getModule(m=>m.default && m.default.displayName == "Message");
+                  if(Message && "default" in Message) this.unpatch = Patcher.after(Message, "default", (_, args, ret)=>{
+                    ret.props.children.props.onDoubleClick = () => {
+                          const props = Utilities.getNestedProp(args[0], "childrenMessageContent.props");
+                          if(!props) return;
+                          if(props && this.settings.edit && !this.settings.delete && props.message.author.id == DiscordAPI.currentUser.id) {
+                              WebpackModules.getByProps("startEditMessage").startEditMessage(props.message.channel_id, props.message.id, props.message.content)
+                          };
+                          if(props && this.settings.delete && !this.settings.edit && !this.settings.copy_link && !this.settings.add_reaction) {
+                              WebpackModules.getByProps("deleteMessage").deleteMessage(props.message.channel_id, props.message.id);
+                          };
+                          if(props && this.settings.copy_link && !this.settings.delete) {
+                              ElectronModule.copy(`https://discordapp.com/channels/${props.channel.guild_id}/${props.channel.id}/${props.message.id}`);
+                              Toasts.success("Copied Message Link");
+                          };
+                          if(props && this.settings.copy_content) {
+                              ElectronModule.copy(props.message.content);
+                              Toasts.success("Copied Message Content");
+                          };
+                          if(props && this.settings.add_reaction && !this.settings.delete) {
+                              WebpackModules.getByProps("addReaction").addReaction(props.message.channel_id, props.message.id, {name: this.settings.reaction})
+                          }    
+                      }
+                  })
+              }
+              onStop() {
+                  this.unpatch();
+              }
+              getSettingsPanel() {
+                  return this.buildSettingsPanel().getElement();
+              }
 
-            }
+          }
 
-        };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
+      };
+      return plugin(Plugin, Api);
+  })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
