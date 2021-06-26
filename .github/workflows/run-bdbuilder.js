e8223c128cea7f0e784ce9688a084c613beb5bdb
square <square3880@gmail.com>
2021-06-26 18:13:00 +0200

    wip pipeline

--- .github/workflows/run-bdbuilder.js
+++ .github/workflows/run-bdbuilder.js
@@ -2,6 +2,7 @@
 
 const _path = require("path");
 const _cp = require("child_process");
+const _fs = require("fs");
 
 const changedFiles = process.argv.slice(2);
 const changedPlugins = [...new Set(changedFiles.map(filename => filename.split(_path.sep)[0]))].filter(pluginName => "." !== pluginName[0]);
@@ -9,8 +10,8 @@ const changedPlugins = [...new Set(changedFiles.map(filename => filename.split(_
 // Needed while bdbuilder is a submodule; obsolete after npm release
 if (0 < changedPlugins.length) {
   _cp.execSync("npm i", {
-    cwd: _path.resolve(process.cwd(), "bdbuilder"),
-    stdio: ["ignore", "inherit", "inherit"],
+    cwd: "bdbuilder",
+    stdio: ["ignore", "inherit", "inherit"]
   });
 }
 
@@ -18,14 +19,18 @@ for (let i = 0; i < changedPlugins.length; i++) {
   buildPlugin(changedPlugins[i]);
 }
 
+_fs.rmSync(_path.resolve(process.cwd(), "bdbuilder/node_modules"), { force: true, recursive: true });
+
+
 function buildPlugin(pluginName) {
-  const pluginSrcDir = _path.resolve(process.cwd(), pluginName, "src");
+  const pluginSrcDir = _path.resolve(pluginName, "src");
   _cp.execSync("npm i", {
     cwd: pluginSrcDir,
-    stdio: ["ignore", "inherit", "inherit"],
+    stdio: ["ignore", "inherit", "inherit"]
   });
   _cp.execSync(`node ./bdbuilder --plugin="${pluginName}" --build`, {
-    cwd: process.cwd(),
-    stdio: ["ignore", "inherit", "inherit"],
+    cwd: ".",
+    stdio: ["ignore", "inherit", "inherit"]
   });
+  _fs.rmSync(_path.resolve(pluginSrcDir, "node_modules"), { force: true, recursive: true });
 }
