5eea112ee681a3d5e55e382db721490b161ee7e3
square <square3880@gmail.com>
2021-06-26 18:01:44 +0200

    wip pipeline

--- .github/workflows/run-bdbuilder.js
+++ .github/workflows/run-bdbuilder.js
@@ -6,19 +6,26 @@ const _cp = require("child_process");
 const changedFiles = process.argv.slice(2);
 const changedPlugins = [...new Set(changedFiles.map(filename => filename.split(_path.sep)[0]))].filter(pluginName => "." !== pluginName[0]);
 
+// Needed while bdbuilder is a submodule; obsolete after npm release
+if (0 < changedPlugins.length) {
+  _cp.execSync("npm i", {
+    cwd: _path.resolve(process.cwd(), "bdbuilder"),
+    stdio: ["ignore", "inherit", "inherit"],
+  });
+}
+
 for (let i = 0; i < changedPlugins.length; i++) {
   buildPlugin(changedPlugins[i]);
 }
 
 function buildPlugin(pluginName) {
-  const pluginDir = _path.resolve(process.cwd(), pluginName);
-  const pluginSrcDir = _path.resolve(pluginDir, "src");
+  const pluginSrcDir = _path.resolve(process.cwd(), pluginName, "src");
   _cp.execSync("npm i", {
     cwd: pluginSrcDir,
-    stdio: ["ignore", "inherit", "inherit"]
+    stdio: ["ignore", "inherit", "inherit"],
   });
   _cp.execSync(`node ./bdbuilder --plugin="${pluginName}" --build`, {
     cwd: process.cwd(),
-    stdio: ["ignore", "inherit", "inherit"]
+    stdio: ["ignore", "inherit", "inherit"],
   });
 }
