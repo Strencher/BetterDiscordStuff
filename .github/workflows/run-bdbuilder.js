a03bf8c5547c674622a26f31a3de1cb7f53323b6
square <square3880@gmail.com>
2021-06-26 17:46:47 +0200

    wip pipeline

---
+++ .github/workflows/run-bdbuilder.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const _path = require("path");
+const _cp = require("child_process");
+
+const changedFiles = process.argv.slice(2);
+const changedPlugins = [...new Set(changedFiles.map(filename => filename.split(_path.sep)[0]))].filter(pluginName => "." !== pluginName[0]);
+
+for (let i = 0; i < changedPlugins.length; i++) {
+  buildPlugin(changedPlugins[i]);
+}
+
+function buildPlugin(pluginName) {
+  const pluginDir = _path.resolve(process.cwd(), pluginName);
+  const pluginSrcDir = _path.resolve(pluginDir, "src");
+  _cp.execSync("npm i", {
+    cwd: pluginSrcDir,
+    stdio: ["ignore", "inherit", "inherit"]
+  });
+  _cp.execSync(`node ./bdbuilder --plugin="${pluginName}" --build`, {
+    cwd: pluginSrcDir,
+    stdio: ["ignore", "inherit", "inherit"]
+  });
+}
