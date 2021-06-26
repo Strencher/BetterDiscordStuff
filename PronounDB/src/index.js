c2344816357fb424211cb7b4fb95282237082eaf
Strencher <46447572+Strencher@users.noreply.github.com>
2021-06-26 04:45:53 -0700

    Update index.js

--- PronounDB/src/index.js
+++ PronounDB/src/index.js
@@ -14,7 +14,6 @@ import createUpdateWrapper from "../../common/hooks/createUpdateWrapper";
 const SelectInput = createUpdateWrapper(WebpackModules.getByProps("SingleSelect").SingleSelect);
 const TextInput = createUpdateWrapper(WebpackModules.getByDisplayName("TextInput"));
 const Header = WebpackModules.getByDisplayName("Header");
-
 export default class PronounDB extends BasePlugin {
     promises = {
         cancelled: false,
