852a73421129ffeca04040ac021fd2139f223990
Strencher <46447572+Strencher@users.noreply.github.com>
2022-07-19 22:43:45 +0200

    ChannelDms v1.2.1

--- ChannelDms/components/privatechannels.tsx
+++ ChannelDms/components/privatechannels.tsx
@@ -2,9 +2,28 @@ import {WebpackModules, Components, Logger} from "@zlibrary";
 import {useCallback} from "react";
 import ChannelInfoContext from "./context";
 import createStore from "common/hooks/zustand";
+import React from "react";
 
 const PrivateChannelsConnected = WebpackModules.getByDisplayName("PrivateChannelsConnected");
 const [useChannelStore, Api] = createStore({shouldShow: true, selectedChannelId: ""});
+
+const original = Symbol("original");
+
+function NestedPrivateChannels(props) {
+    const res = props[original](props);
+
+    try {
+        const items = res?.props?.children?.[1]?.props?.children;
+
+        if (Array.isArray(items)) {
+            while (items.length) items.pop();
+        }
+    } catch (error) {
+        Logger.error("Failed to patch NestedPrivateChannels:", error);
+    }
+
+    return res;
+}
 
 function PrivateChannelsPatched(props: any) {
     const ret = PrivateChannelsConnected(props);
@@ -12,7 +31,9 @@ function PrivateChannelsPatched(props: any) {
     try {
         ret.props.showNitroTab = false;
         ret.props.showLibrary = false;
-        ret.props.homeLink = null;
+        ret.props.homeLink = "";
+        ret.props[original] = ret.type;
+        ret.type = NestedPrivateChannels;
     } catch (error) {
         Logger.error(`Failed to set properties on PrivateChannels:`, error);
     }
