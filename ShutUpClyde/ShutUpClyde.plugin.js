93608e8d64458465845e1ee0586d700961ced816
Strencher <46447572+Strencher@users.noreply.github.com>
2023-08-31 21:02:06 +0200

    Migrate to new meta style & drop zlibrary dep

--- ShutUpClyde/ShutUpClyde.plugin.js
+++ ShutUpClyde/ShutUpClyde.plugin.js
@@ -1,96 +1,23 @@
-//META{"name":"ShutUpClyde","displayName":"ShutUpClyde"}*//
-/*@cc_on
-@if (@_jscript)
-    
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+/**
+ * @name ShutUpClyde
+ * @version 1.0.1
+ * @author Strencher
+ * @description Prevents Clyde from disturbing you.
+ * @source https://github.com/Strencher/BetterDiscordStuff/ShutUpClyde/ShutUpClyde.plugin.js
+ * @donate https://paypal.me/RealStrencher
+ */
+
+const {Patcher, Webpack} = new BdApi("ShutUpClyde");
+
+module.exports = () => ({
+    start() {
+        const MessageActions = Webpack.getByKeys("sendBotMessage");
+        
+        Patcher.instead(MessageActions, "sendBotMessage", () => {});
+        Patcher.instead(MessageActions, "sendClydeError", () => {});
+    },
+
+    stop() {
+        Patcher.unpatchAll();
     }
-    WScript.Quit();
-
-@else@*/
-
-module.exports = (() => {
-    const config = {
-        info: {
-            name: "ShutUpClyde",
-            authors: [
-                {
-                    name: "Strencher",
-                    discord_id: "415849376598982656",
-                    github_username: "Strencher",
-                    twitter_username: "Strencher3"
-                }
-            ],
-            version: "0.0.1",
-            description: "Prevents Clyde from disturbing you.",
-            github: "https://github.com/Strencher/BetterDiscordStuff/ShutUpClyde/ShutUpClyde.plugin.js",
-            github_raw: "https://raw.githubusercontent.com/Strencher/BetterDiscordStuff/master/ShutUpClyde/ShutUpClyde.plugin.js"
-        },
-        changelog: [
-            {
-                title: "Yeah",
-                type: "added",
-                items: ["The plugin exist"]
-            }
-        ]
-    };
-
-    return !global.ZeresPluginLibrary ? class {
-        constructor() { this._config = config; }
-        getName() { return config.info.name; }
-        getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
-        getDescription() { return config.info.description; }
-        getVersion() { return config.info.version; }
-        load() {
-            BdApi.showConfirmationModal("Library plugin is needed", 
-                [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`], {
-                    confirmText: "Download",
-                    cancelText: "Cancel",
-                    onConfirm: () => {
-                        require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
-                        });
-                    }
-                });
-        }
-        start() { }
-        stop() { }
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Api) => {
-
-            const { WebpackModules, DiscordModules, Patcher } = Api;
-            return class ShutUpClyde extends Plugin {
-                constructor() {
-                    super();
-                }
-
-                onStart() { 
-                    Patcher.instead(DiscordModules.MessageActions, "sendBotMessage", () => {})
-                    Patcher.instead(WebpackModules.getByProps("sendClydeError"), "sendClydeError", () => {})
-                }
-                onStop() {
-                    Patcher.unpatchAll()
-                }
-
-            }
-
-        };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
-/*@end@*/
+});
