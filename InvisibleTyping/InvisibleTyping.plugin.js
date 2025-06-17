fdddbcae5c7358c64e0dbe4524d657e2b17f2902
PluginBuilder <actions@github.com>
2025-06-17 21:45:17 +0000

    Publish build

--- InvisibleTyping/InvisibleTyping.plugin.js
+++ InvisibleTyping/InvisibleTyping.plugin.js
@@ -422,7 +422,7 @@ class InvisibleTyping {
     patchChannelTextArea() {
         const ChatButtonsGroup = Webpack.getBySource('"ChannelTextAreaButtons"').Z;
         Patcher.after(ChatButtonsGroup, "type", (_, args, res) => {
-            if (args.length == 2 && res.props.children && Array.isArray(res.props.children)) {
+            if (args.length == 2 && !args[0].disabled && res.props.children && Array.isArray(res.props.children)) {
                 res.props.children.unshift(React.createElement(InvisibleTypingButton, {
                     channel: args[0].channel,
                     isEmpty: !Boolean(args[0].textValue)
