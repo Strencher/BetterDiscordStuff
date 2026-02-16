e0f831a8a38b4780b2e7c8a0fc25804dd60ae089
Dominik <me@domi-btnr.dev>
2026-02-16 12:39:07 +0100

    [PlatformIndicators] Support for VR

--- PlatformIndicators/components/icons/Icons.js
+++ PlatformIndicators/components/icons/Icons.js
@@ -1,4 +1,5 @@
 export {default as desktop} from "./desktop";
-export {default as mobile} from "./mobile";
 export {default as embedded} from "./embedded";
+export {default as mobile} from "./mobile";
+export {default as vr} from "./vr";
 export {default as web} from "./web";
