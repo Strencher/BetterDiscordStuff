eddbfed8d8c40d745b3f264d4fa6b3acbf5d23a0
Dominik <me@domi-btnr.dev>
2026-02-27 09:29:36 +0100

    Update VR Key

--- PlatformIndicators/modules/utils.js
+++ PlatformIndicators/modules/utils.js
@@ -7,7 +7,7 @@ export const findInReactTree = (tree, filter) => Utils.findInTree(tree, filter, 
 function upperFirst(string) {return string.charAt(0).toUpperCase() + string.slice(1);}
 
 export function getStatusText(key, status) {
-    return upperFirst(key) + ": " + Messages[`STATUS_${(status === "mobile" ? "mobile_online" : status).toUpperCase()}`];
+    return (key === "vr" ? "VR" : upperFirst(key)) + ": " + Messages[`STATUS_${(status === "mobile" ? "mobile_online" : status).toUpperCase()}`];
 }
 
 export function getStatusColor(status) {
