057991f0c7870488fe83412199474ff8781f449e
Dominik <me@domi-btnr.dev>
2026-05-15 11:39:03 +0200

    🥅 Wrap Settings in ErrorBoundary

--- common/Settings/panel.tsx
+++ common/Settings/panel.tsx
@@ -1,52 +1,62 @@
 import { Components, Hooks, Webpack } from "@api";
-import { Select as SelectType, Slider as SliderType } from "@vencord/discord-types";
+import ErrorBoundary from "@common/ErrorBoundary";
+import { Select as SelectType, SelectOption, Slider as SliderType } from "@vencord/discord-types";
 import React from "react";
 
 import Settings from "./store";
 import type { SettingsItem, SettingsItemForType } from "./types";
 
 const { SettingItem, SwitchInput } = Components;
-const Select: SelectType = Webpack.getByStrings('.selectPositionTop]:"top"===', { searchExports: true });
+const Select: SelectType = Webpack.getByStrings('selectionMode:"single",onSelectionChange:', "isSelected:", {
+    searchExports: true
+});
 const Slider: SliderType = Webpack.getByStrings("stickToMarkers");
 
 function DropdownItem(props: SettingsItemForType<"dropdown">) {
     return (
-        <SettingItem {...props}>
-            <Select
-                closeOnSelect={true}
-                options={props.options}
-                serialize={v => String(v)}
-                select={v => Settings.set(props.id, v)}
-                isSelected={v => Settings.get(props.id, props.value) === v}
-            />
-        </SettingItem>
+        <ErrorBoundary key={props.id} id={props.id}>
+            <SettingItem {...props}>
+                <Select
+                    closeOnSelect={true}
+                    options={props.options as SelectOption[]}
+                    serialize={v => String(v)}
+                    select={v => Settings.set(props.id, v)}
+                    isSelected={v => Settings.get(props.id, props.value) === v}
+                />
+            </SettingItem>
+        </ErrorBoundary>
     );
 }
 
 function SwitchItem(props: SettingsItemForType<"switch">) {
     const value = Hooks.useStateFromStores([Settings], () => Settings.get(props.id, props.value));
     return (
-        <SettingItem {...props} inline={true}>
-            <SwitchInput value={value} onChange={v => Settings.set(props.id, v)} />
-        </SettingItem>
+        <ErrorBoundary key={props.id} id={props.id}>
+            <SettingItem {...props} inline={true}>
+                <SwitchInput value={value} onChange={v => Settings.set(props.id, v)} />
+            </SettingItem>
+        </ErrorBoundary>
     );
 }
 
 function SliderItem(props: SettingsItemForType<"slider">) {
+    if (!Slider) return null;
     const value = Hooks.useStateFromStores([Settings], () => Settings.get(props.id, props.value));
     return (
-        <SettingItem {...props}>
-            <Slider
-                {...props}
-                handleSize={10}
-                initialValue={value}
-                defaultValue={props.defaultValue}
-                minValue={props.minValue}
-                maxValue={props.maxValue}
-                onValueChange={(value: number) => Settings.set(props.id, Math.round(value))}
-                onValueRender={(value: number) => Math.round(value)}
-            />
-        </SettingItem>
+        <ErrorBoundary key={props.id} id={props.id}>
+            <SettingItem {...props}>
+                <Slider
+                    {...props}
+                    handleSize={10}
+                    initialValue={value}
+                    defaultValue={props.defaultValue}
+                    minValue={props.minValue}
+                    maxValue={props.maxValue}
+                    onValueChange={(value: number) => Settings.set(props.id, Math.round(value))}
+                    onValueRender={(value: number) => Math.round(value)}
+                />
+            </SettingItem>
+        </ErrorBoundary>
     );
 }
 
